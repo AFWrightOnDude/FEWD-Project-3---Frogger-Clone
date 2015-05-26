ff20a8a99c230ad3d72fb034e1d9e177f32bfe6e
Aaron Wright <aaronwright@Aarons-MacBook-Pro.local>
2015-05-25 22:24:17 -0400

    First commit

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
-frontend-nanodegree-arcade-game
-===============================
+Welcome to the world of Buginator!
 
-Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
+Collect water to get points!
+But watch out...the evil bugs will nibble away your points!
+Can you avoid the evil bugs and move your player to the magic water to collect the
+most points?!
 
-for self-checking their submission.
+To play Buginator:
+
+>>Download the following files/folders for Buginator and save them to your favorite spot
+on your computer:
+	Index.html
+	css (folder)
+	images (folder)
+	js (folder)
+
+>>Launch Buginator by opening index.html in your favorite Web browser such as Chrome or Firefox- double clicking it should do!
+
+>>Use the arrow keys to navigate our heroine Pointy Hat Girl to reach the Magic Water..but watch out for the bugs! If you have points, they will eat one when they catch you!
+
+>>When you reach the water, you will score a point and warp back to the start. Go and get more points!
+
+Brought to you by Udacity and Aaron Wright (with a lot of help from other Udacians and some Googling!)
--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+var Enemy = function(x,y) {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = getRandomSpeed(); //give each bug its own speed!
+}
+
+function getRandomSpeed() {
+    return Math.random() * (600 - 300) + 50; //calculates a random speed for the bug
 }
 
 // Update the enemy's position, required method for game
@@ -14,22 +18,98 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if (collisionChecker){
+            player.y = 380;           //reset player position if collision detected
+            player.x = 205;
+            if (score > 0){
+                score = score - 1;      //penalize player 1 point if they have any
+                document.getElementById("scoreboard").innerHTML ="Score:" + " " + score;
+                alert('You just got BUGINATED!! The bug ate one of your points! :(');
+            } else {
+                alert('You just got BUGINATED!!');
+            }
+        collisionChecker = false;  //reset the collision checker
+
+    }else if (this.x < 510){
+        this.x = this.x + this.speed * dt; //if the bug is not past the screen we move it right
+    } else {
+        this.x = -50; //if the bug is past the right edge, we reset its position to the left
+        //randomly change the row that the bug is on
+        this.y = stoneRowYCordinate[Math.floor(Math.random()*stoneRowYCordinate.length)];
+    }
+    if (player.y === this.y && (player.x < this.x +60) && (player.x + 60 > this.x)){
+        collisionChecker = true;
+    }
+
 }
+
+var collisionChecker = false;
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//store the Y cordinates of the stone row centers for chaning the bug's row position on update
+var stoneRowYCordinate = [52, 134, 216];
 
+//let's make some new bugs!
+var allEnemies = [];
+allEnemies[0]= new Enemy(40,52);
+allEnemies[1]= new Enemy(-40,52);
+allEnemies[2]= new Enemy(40,134);
+allEnemies[3]= new Enemy(120,216);
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var Player = function() {
+    this.sprite = 'images/char-horn-girl.png'; //set the players sprite image
+    this.x = 205; // x axis starting position
+    this.y = 380;// y axist starting position..each row is 82px apart
+};
 
+Player.prototype.update = function () {
+    //I may have had some code that could go more efficiently here...but I could not
+    //think of anything. Any suggestions for the sake of learning? Thanks!
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var score = 0; // set a variable to track the score- how many times the player reaches water
+
+//this block of code moves the player based upon key input received from handleInput
+Player.prototype.handleInput = function (key) {
+      switch(key){
+        case 'left' :
+            if (this.x > 37){  //checks to make sure player is not moving past left edge
+            this.x = this.x - 100;
+            }
+            break;
+            case 'right' :
+            if (this.x < 373){ //checkes to make sure player is not moving past right edge
+            this.x = this.x + 100;
+            }
+            break;
+            case 'down' :
+            if (this.y < 380){ //checks to make sure player is not moving past bottom edge
+            this.y = this.y + 82;
+            }
+            break;
+            case 'up' :
+            if (this.y > 52){ //checks to make sure player has not reached "water" before moving
+            this.y = this.y - 82;
+            }else if (this.y = 52){ //checks to see if next move would put player in water
+            this.y = 380;           //if so, we move the player back to start (reset position)
+            this.x = 205;
+            score = score + 1;      //increment the score and update the score displayed
+            document.getElementById("scoreboard").innerHTML ="Score:" + " " + score;
+            }
+            break;
+        }
+}
+
+//make a new player object
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
